68483777874c1fa4756421cbcc5ed4e9f87d7cad
The Lú <71472325+The-Lu33@users.noreply.github.com>
2022-10-11 07:48:43 -0500

    style de main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,66 @@
 const items = [
-    {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'assets/images/featured1.png',
-      category: 'hoodies',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'assets/images/featured2.png',
-      category: 'shirts',
-      quantity: 15
-    },
-    {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'assets/images/featured3.png',
-      category: 'shirts',
-      quantity: 20
-    }
-  ]
+  {
+    id: 1,
+    name: "Hoodies",
+    price: 14.0,
+    image: "assets/images/featured1.png",
+    category: "hoodies",
+    quantity: 10,
+  },
+  {
+    id: 2,
+    name: "Shirts",
+    price: 24.0,
+    image: "assets/images/featured2.png",
+    category: "shirts",
+    quantity: 15,
+  },
+  {
+    id: 3,
+    name: "Sweatshirts",
+    price: 24.0,
+    image: "assets/images/featured3.png",
+    category: "shirts",
+    quantity: 20,
+  },
+];
 
-  /*LODER */
-  const loadComponent = () => {
-    const loader = document.getElementById( "loader" )
+/*LODER */
+const loadComponent = () => {
+  const loader = document.getElementById("loader");
 
-    setTimeout(() => {
-        loader.classList.add( "hide" )
-    }, 3000);
-}
+  setTimeout(() => {
+    loader.classList.add("hide");
+  }, 3000);
+};
 
-
-document.addEventListener( "DOMContentLoaded", () =>{
-    loadComponent() 
-})
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent();
+});
 
 /*CABEZA */
-const themeDark = document.getElementById("id--theme__dark")
+const themeDark = document.getElementById("id--theme__dark");
 console.log(themeDark);
 
-themeDark.addEventListener( "click", () => {
-  document.body.classList.toggle("theme__dark")
-    if( themeDark.classList.contains("bx-moon") ){ //evaluar si existe la clase bx-moon
-        themeDark.classList.replace("bx-moon", "bx-sun")
-    }else{
-        themeDark.classList.replace("bx-sun", "bx-moon")
-    }
-})
+themeDark.addEventListener("click", () => {
+  document.body.classList.toggle("theme__dark");
+  if (themeDark.classList.contains("bx-moon")) {
+    //evaluar si existe la clase bx-moon
+    themeDark.classList.replace("bx-moon", "bx-sun");
+  } else {
+    themeDark.classList.replace("bx-sun", "bx-moon");
+  }
+});
 
-
-const closeMain = document.getElementById("navOpen")
+const closeMain = document.getElementById("navOpen");
 
 console.log(closeMain);
 
-closeMain.addEventListener( "click", () => {
-  const cerrar = document.getElementById("navClose")
+closeMain.addEventListener("click", () => {
+  const cerrar = document.getElementById("navClose");
   console.log(cerrar);
-  cerrar.classList.add( "navCloseActive" )
-
-    
-})
-
+  cerrar.classList.add("navCloseActive");
+});
 
 /** section products */
 const showProducts = () => {
@@ -75,12 +71,16 @@ const showProducts = () => {
   items.forEach((producto) => {
     fragment += `
         <div class="card--product" id="${producto.id}">
-
+        <div class="card--product__img">
         <img src="${producto.image}" alt="">
+        </div>
         <button class="btn-add">+</button>
         <div class="details">
-        <h2 class="price-card">$${producto.price} </h2>
-            <p>stock${producto.quantity}</p>
+        <h2 class="price-card">${new Intl.NumberFormat("en-IN", {
+          style: "currency",
+          currency: "USD",
+        }).format(producto.price)} </h2>
+            <p class="text--stock">stock${producto.quantity}</p>
 
             <p>${producto.name}</p>
             </div>
@@ -93,7 +93,22 @@ const showProducts = () => {
   // cartfuntion();
 };
 
+// const counterProduts = () => {
+//   const linkShop = document.querySelectorAll(".container--link--shop");
+//   let conterItems = ``;
+//   items.forEach((producto) => {
+//     conterItems += `
+//  <div class="all--shop">
+//  <h2>${producto.name}</h2>
+//  <p> ${producto.quantity} products</p>
+// </div>
+//   `;
+//   });
 
-document.addEventListener('DOMContentLoaded', () => {
-  showProducts()
-})
+//   linkShop.innerHTML = conterItems;
+//   console.log(linkShop);
+// };
+
+document.addEventListener("DOMContentLoaded", () => {
+  showProducts();
+});
