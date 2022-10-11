2204631d2b63fa314b692c53907c2b16872775bb
The Lú <71472325+The-Lu33@users.noreply.github.com>
2022-10-10 23:33:31 -0500

    se hizo un reset para organizar bn las raices, se coloca head y main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,42 @@
 const items = [
-    {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'assets/images/featured1.png',
-      category: 'hoodies',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'assets/images/featured2.png',
-      category: 'shirts',
-      quantity: 15
-    },
-    {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'assets/images/featured3.png',
-      category: 'shirts',
-      quantity: 20
-    }
-  ]
+  {
+    id: 1,
+    name: 'Hoodies',
+    price: 14.00,
+    image: 'assets/images/featured1.png',
+    category: 'hoodies',
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'Shirts',
+    price: 24.00,
+    image: 'assets/images/featured2.png',
+    category: 'shirts',
+    quantity: 15
+  },
+  {
+    id: 3,
+    name: 'Sweatshirts',
+    price: 24.00,
+    image: 'assets/images/featured3.png',
+    category: 'shirts',
+    quantity: 20
+  }
+]
 
-  /*LODER */
-  const loadComponent = () => {
-    const loader = document.getElementById( "loader" )
+/*LODER */
+const loadComponent = () => {
+  const loader = document.getElementById( "loader" )
 
-    setTimeout(() => {
-        loader.classList.add( "hide" )
-    }, 3000);
+  setTimeout(() => {
+      loader.classList.add( "hide" )
+  }, 3000);
 }
 
 
 document.addEventListener( "DOMContentLoaded", () =>{
-    loadComponent() 
+  loadComponent() 
 })
 
 /*CABEZA */
@@ -44,12 +44,12 @@ const themeDark = document.getElementById("id--theme__dark")
 console.log(themeDark);
 
 themeDark.addEventListener( "click", () => {
-  document.body.classList.toggle("theme__dark")
-    if( themeDark.classList.contains("bx-moon") ){ //evaluar si existe la clase bx-moon
-        themeDark.classList.replace("bx-moon", "bx-sun")
-    }else{
-        themeDark.classList.replace("bx-sun", "bx-moon")
-    }
+document.body.classList.toggle("theme__dark")
+  if( themeDark.classList.contains("bx-moon") ){ //evaluar si existe la clase bx-moon
+      themeDark.classList.replace("bx-moon", "bx-sun")
+  }else{
+      themeDark.classList.replace("bx-sun", "bx-moon")
+  }
 })
 
 
@@ -58,9 +58,9 @@ const closeMain = document.getElementById("navOpen")
 console.log(closeMain);
 
 closeMain.addEventListener( "click", () => {
-  const cerrar = document.getElementById("navClose")
-  console.log(cerrar);
-  cerrar.classList.add( "navCloseActive" )
+const cerrar = document.getElementById("navClose")
+console.log(cerrar);
+cerrar.classList.add( "navCloseActive" )
 
-    
+  
 })
